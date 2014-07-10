6d8f2b90c5c4806f3bcc79b0404c0c4ab6899062
Simon Kennedy <code@sffjunkie.co.uk>
2014-07-10 12:20:03 +0100

    ColorVar now stores its value by pickling via a new PickleVar base class.

--- tks/vars.py
+++ tks/vars.py
@@ -1,6 +1,15 @@
 # Copyright 2014, Simon Kennedy, sffjunkie+code@gmail.com
 
-"""A 3 element color variable."""
+"""tks variable classes.
+
+ColorVar
+    An RGB color
+
+DateTimeVar
+
+"""
+
+import pickle
 
 try:
     import tkinter as tk
@@ -13,7 +22,21 @@ except NameError:
     StringType = str
 
 
-class ColorVar(tk.Variable, object):
+class PickleVar(tk.Variable, object):
+    def __init__(self, master=None, value=None, name=None):
+        super(PickleVar, self).__init__(master, value, name)
+
+    def get(self):
+        """Get the stored value."""
+
+        value = self._tk.globalgetvar(self._name)
+        return pickle.loads(value)
+
+    def set(self, value):
+        return tk.Variable.set(self, pickle.dumps(value))
+
+
+class ColorVar(PickleVar):
     """A Tkinter Variable subclass to store an RGB color value."""
 
     def __init__(self, master=None, value=None, name=None):
@@ -24,20 +47,18 @@ class ColorVar(tk.Variable, object):
 
         super(ColorVar, self).__init__(master, value, name)
 
-    def get(self):
-        """Get the stored color value."""
+    def set(self, value):
+        """Set the color tuple to be stored."""
 
-        value = self._tk.globalgetvar(self._name)
-        if isinstance(value, tuple):
-            return value
-        else:
-            return eval(str(value))
+        value = tuple([float(x) for x in value[:3]])
 
-    def set(self, color_tuple):
-        """Set the color color_tuple to be stored."""
+        factorize = False
+        for elem in value:
+            if elem > 1.0:
+                factorize = True
+                break
 
-        if isinstance(color_tuple, StringType):
-            color_tuple = eval(color_tuple)
+        if factorize:
+            value = tuple([x / 255.0 for x in value])
 
-        color_tuple = tuple([float(x) for x in color_tuple[:3]])
-        return tk.Variable.set(self, color_tuple)
+        return super(ColorVar, self).set(value)
