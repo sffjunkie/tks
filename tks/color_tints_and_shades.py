0657c29274ea6aaf114367abe9de9727b2a65eda
Simon Kennedy <code@sffjunkie.co.uk>
2014-07-16 10:44:23 +0100

    Changed the way imports are done.

--- tks/color_tints_and_shades.py
+++ tks/color_tints_and_shades.py
@@ -11,24 +11,22 @@ ColorShade - Displays a set of shades
 """
 
 from __future__ import print_function, division, absolute_import
+import sys
 import math
 
-try:
+if sys.version_info >= (3, 0):
     import tkinter as tk
-except ImportError:
-    import Tkinter as tk
-
-try:
     from tkinter import ttk
-except ImportError:
+else:
+    import Tkinter as tk
     import ttk
 
 from .i18n import language
 _ = language.gettext
 
-from tks.vars import ColorVar
-from tks.color_square import ColorSquare
-from tks.color_funcs import rgb_tints, rgb_shades
+import tks.colors
+import tks.color_funcs
+import tks.color_square
 
 __all__ = ['ColorTint', 'ColorShade']
 
@@ -51,7 +49,7 @@ class _TintAndShadeBase(ttk.Frame, object):
             self.color_var = variable
             self.color_var.trace_variable('w', self._color_var_changed)
         else:
-            self.color_var = ColorVar()
+            self.color_var = tks.colors.ColorVar()
 
         self._count = count
 
@@ -59,10 +57,10 @@ class _TintAndShadeBase(ttk.Frame, object):
 
         self._tint_squares = []
         for idx in range(self._count):
-            sq = ColorSquare(frame,
-                             variable=self.color_var,
-                             mode='w',
-                             dnd_target=False)
+            sq = tks.color_square.ColorSquare(frame,
+                                              variable=self.color_var,
+                                              mode='w',
+                                              dnd_target=False)
             sq.grid(row=0, column=idx, sticky=tk.NE, padx=4, pady=4)
             self._tint_squares.append(sq)
 
@@ -136,7 +134,7 @@ class ColorTint(_TintAndShadeBase):
         super(ColorTint, self).__init__(master, variable, _('Tints'),
                                         count=count,
                                         percent=percent,
-                                        func=rgb_tints)
+                                        func=tks.color_funcs.rgb_tints)
 
 
 class ColorShade(_TintAndShadeBase):
@@ -158,4 +156,4 @@ class ColorShade(_TintAndShadeBase):
         super(ColorShade, self).__init__(master, variable, _('Shades'),
                                          count=count,
                                          percent=percent,
-                                         func=rgb_shades)
+                                         func=color_funcs.rgb_shades)
