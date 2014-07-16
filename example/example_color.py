3b2d6cb622b9ecce85db1f11eac9c705cfbe4e7b
Simon Kennedy <code@sffjunkie.co.uk>
2014-07-16 11:06:13 +0100

    Added example code to pass a variable to the `ColorEntry`

--- example/example_color.py
+++ example/example_color.py
@@ -10,12 +10,16 @@ try:
 except ImportError:
     import Tkinter as tk
 
-from tks.colors import ColorEntry
+import tks.colors
+import tks.color_funcs
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title('Color Test')
-    entry = ColorEntry(root, start_color='#fedcba')
+
+    var = tks.colors.ColorVar(value=tks.color_funcs.hex_string_to_rgb('#fedcba'))
+    entry = tks.colors.ColorEntry(root, variable=var, color_format='rgbhex')
     entry.grid(row=0, column=0, sticky=tk.EW)
+
     root.columnconfigure(0, weight=1)
     root.mainloop()
