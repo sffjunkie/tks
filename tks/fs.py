83576b106a9b096e681f39f41dd98a1345e69872
Simon Kennedy <code@sffjunkie.co.uk>
2014-07-16 10:33:39 +0100

    * Changed `shorten` from a method of FSEntry to a function. * `shorten` now shortens using current directory first then home directory. * FSEntry now takes a Tk variable as an argument for the text in the entry box. * Added a function to control when a tooltip is displayed.

--- tks/fs.py
+++ tks/fs.py
@@ -16,74 +16,92 @@ import os
 import sys
 import os.path
 
-try:
+if sys.version_info >= (3, 0):
     import tkinter as tk
-except ImportError:
-    import Tkinter as tk
-
-try:
     from tkinter import ttk
-except ImportError:
-    import ttk
-
-try:
     from tkinter import filedialog
-except ImportError:
+else:
+    import Tkinter as tk
+    import ttk
     import tkFileDialog as filedialog
+
+from tks.tooltip import ToolTip
+
+
+def shorten(path):
+    """Shorten a path.
+
+    If the path starts with the path to the home directory then replace
+    it with the ``~`` character.
+
+    If the path starts with the current directory then replace it with
+    the ``.`` character
+    """
+
+    home_dir = os.path.expanduser('~')
+
+    if 'win32' in sys.platform:
+        path = path.replace('/', '\\')
+
+    current_dir = os.getcwd()
+    if path.startswith(current_dir):
+        path = os.path.join('.', path[len(current_dir) + 1:])
+
+    if path.startswith(home_dir):
+        path = os.path.join('~', path[len(home_dir) + 1:])
+
+    return path
 
 
 class FSEntry(ttk.Frame, object):
     """Base class for filesystem entries."""
 
-    def __init__(self, master):
+    def __init__(self, master,
+                 variable=None):
         self.master = master
         super(FSEntry, self).__init__(master)
 
-        self._var = tk.StringVar()
+        if variable:
+            self._variable = variable
+        else:
+            self._variable = tk.StringVar()
 
-        self._entry = ttk.Entry(self, textvariable=self._var)
+        self._full_path = ''
+
+        self._entry = ttk.Entry(self, textvariable=self._variable)
         self._entry.grid(row=0, column=0, sticky=tk.EW)
 
         self._btn = ttk.Button(self, text='Browse...',
-                               command=self.browse)
+                               command=self._browse)
         self._btn.grid(row=0, column=1, sticky=tk.E, padx=(6, 0))
+
+        self._tooltip = ToolTip(self._entry,
+                                display_func=self._tooltip_display_func,
+                                msg_func=self._tooltip_msg_func)
 
         self.columnconfigure(0, weight=1)
 
     @property
     def value(self):
-        return self._var.get()
+        return self._variable.get()
 
     @value.setter
     def value(self, value):
-        self._var.set(str(value))
+        self._variable.set(str(value))
 
-    def browse(self):
+    def _browse(self):
         """Browse for an entry. To be overridden by the subclass."""
 
         raise NotImplementedError
 
-    def shorten(self, path):
-        """Shorten a path.
+    def _tooltip_display_func(self):
+        return bool(self._tooltip_msg_func())
 
-        If the path starts with the path to the home directory then replace
-        it with the ``~`` character.
+    def _tooltip_msg_func(self, *args):
+        path = self._variable.get()
 
-        If the path starts with the current directory then replace it with
-        the ``.`` character
-        """
-
-        home_dir = os.path.expanduser('~')
-
-        if 'win32' in sys.platform:
-            path = path.replace('/', '\\')
-
-        if path.startswith(home_dir):
-            path = os.path.join('~', path[len(home_dir) + 1:])
-
-        current_dir = os.getcwd()
-        if path.startswith(current_dir):
-            path = os.path.join('.', path[len(current_dir) + 1:])
+        if not path:
+            return None
 
         return path
 
@@ -96,10 +114,10 @@ class DirEntry(FSEntry):
         self.options = options
         super(DirEntry, self).__init__(master, variable)
 
-    def browse(self):
-        """Display the dialog to browse for a directory."""
+    def _browse(self):
+        """Display the dialog to _browse for a directory."""
 
-        initial_dir = self._var.get()
+        initial_dir = self._variable.get()
         initial_dir = os.path.abspath(os.path.expanduser(initial_dir))
 
         self.options['initialdir'] = initial_dir
@@ -107,7 +125,7 @@ class DirEntry(FSEntry):
         new_dir = dlg.show()
 
         if new_dir:
-            new_dir = self.shorten(new_dir)
+            new_dir = shorten(new_dir)
             self.value = new_dir
 
 
@@ -120,10 +138,10 @@ class FileEntry(FSEntry):
         self.options = options
         super(FileEntry, self).__init__(master, variable)
 
-    def browse(self):
-        """Display the dialog to browse for a file."""
+    def _browse(self):
+        """Display the dialog to _browse for a file."""
 
-        initial_file = self._var.get()
+        initial_file = self._variable.get()
 
         if initial_file:
             initial_dir = os.path.split(initial_file)[0]
@@ -137,6 +155,6 @@ class FileEntry(FSEntry):
         new_file = dlg.show()
 
         if new_file:
-            new_file = self.shorten(new_file)
-            self._var.set(new_file)
+            new_file = shorten(new_file)
+            self.value = new_file
 
